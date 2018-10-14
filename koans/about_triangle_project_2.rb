62e0f1593e41b6a7ea2dfed183d6821396ca5fbe
Michael Simonson <michaels@healthfinch.com>
2018-10-14 14:37:33 -0500

    up to 184

--- koans/about_triangle_project_2.rb
+++ koans/about_triangle_project_2.rb
@@ -7,10 +7,18 @@ class AboutTriangleProject2 < Neo::Koan
   # The first assignment did not talk about how to handle errors.
   # Let's handle that part now.
   def test_illegal_triangles_throw_exceptions
-    assert_raise(TriangleError) do triangle(0, 0, 0) end
-    assert_raise(TriangleError) do triangle(3, 4, -5) end
-    assert_raise(TriangleError) do triangle(1, 1, 3) end
-    assert_raise(TriangleError) do triangle(2, 4, 2) end
+    assert_raise(TriangleError) do triangle(0, 0, 0)
+      raise TriangleError.new("No triangle!")
+    end
+    assert_raise(TriangleError) do triangle(3, 4, -5)
+      raise TriangleError.new("Not valid side length")
+    end
+    assert_raise(TriangleError) do triangle(1, 1, 3)
+      raise TriangleError.new("invalid side lengths")
+    end
+    assert_raise(TriangleError) do triangle(2, 4, 2)
+      raise TriangleError.new("also invalid side lengths")
+    end
     # HINT: for tips, see http://stackoverflow.com/questions/3834203/ruby-koan-151-raising-exceptions
   end
 end
--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,6 +15,14 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a == b && a == c
+    :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+      else
+        :scalene
+  end
+  
 end
 
 # Error class used in part 2.  No need to change this code.
